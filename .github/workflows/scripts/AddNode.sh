81b50ed487294e30e107ce23e40968b5a1c61bba
Iker Ruiz de Infante Gonzalez <iker@alastria.io>
2022-06-20 11:57:20 +0200

    Add nodes to directory files for prometheus

--- .github/workflows/scripts/AddNode.sh
+++ .github/workflows/scripts/AddNode.sh
@@ -3,17 +3,17 @@
 case $TYPE in
 	"bootnode")
 		echo "| $NAME | $HOSTING | - | $ENODE |" >> ./DIRECTORY_BOOTNODES.md
-		echo "| $NAME | $ENODE |" >> ./DIRECTORY_CORE.md
+		echo "$NAME $ENODE" >> ./directory.bot
 		echo "\"$ENODE\"," >> ./data/boot-nodes.json
 	;;
 	"validator")
 		echo "| $NAME | $HOSTING | $ENODE | $ADDRESS | True |" >> ./DIRECTORY_VALIDATOR.md
-		echo "| $NAME | $ENODE |" >> ./DIRECTORY_CORE.md
+		echo "$NAME $ENODE" >> ./directory.val
 		echo "\"$ENODE\"," >> ./data/validator-nodes.json
 	;;
 	"regular")
 		echo "| $NAME | $HOSTING | - | $ENODE |" >> ./DIRECTORY_REGULAR.md
-		echo "| $NAME | $ENODE |" >> ./DIRECTORY.md
+		echo "$NAME $ENODE" >> ./directory.reg
 		echo "\"$ENODE\"," >> ./data/regular-nodes.json
 	;;
 esac
